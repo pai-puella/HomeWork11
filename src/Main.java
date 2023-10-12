fb9becfe519199aa8f486f05bc4ac79a94599193
pai-puella <cat.dyshkant@yandex.ru>
2023-10-12 15:32:49 +0300

    Added Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        int year = 2021;
+        printLeapYear(year);
+    }
+
+    public static void printLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
 }
